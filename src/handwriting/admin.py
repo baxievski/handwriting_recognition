7e16b4dd3a99c4a4ade746c469a2189d9fcfbff3
Bojan Adzievski <bojan.axievski@gmail.com>
2019-09-28 14:43:56 +0200

    add django admin list_filter

--- src/handwriting/admin.py
+++ src/handwriting/admin.py
@@ -8,16 +8,23 @@ from .models import RawInputData, Character
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
-    list_display = ('get_info', )
+    list_display = ('get_image', 'rotation_angle', 'resized_image_dimmensions', 'is_discarded' )
+    list_filter = ("rotation_angle", "raw_input_data__label", "raw_input_data__discarded", )
+    readonly_fields = ("image_data", "resized_image_dimmensions", "rotation_angle")
 
-    def get_info(self, obj):
+    def get_image(self, obj):
         fk_id = obj.raw_input_data.id
         angle = obj.rotation_angle
         width = obj.resized_image_dimmensions[0]
         height = obj.resized_image_dimmensions[1]
         label = obj.raw_input_data.label
-        img_src = f'<img src="/images/{fk_id}/{angle}" width="{width}" height="{height}" /> {label}; {angle}°; {width}x{height}'
+        img_src = f'<img src="/images/{fk_id}/{angle}" width="{width}" height="{height}" /> Label: "{label}"'
         return mark_safe(img_src)
+    
+    def is_discarded(self, obj):
+        return obj.raw_input_data.discarded
 
 
-admin.site.register(RawInputData)
+@admin.register(RawInputData)
+class RawInputDataAdmin(admin.ModelAdmin):
+    readonly_fields = ("image_data", "original_image_dimmensions", "bounding_box")
